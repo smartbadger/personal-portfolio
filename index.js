ddfe62499ed6851ec5e336435f9915b0954cb510
PopSockets <web@popsockets.com>
2018-05-29 21:56:00 -0600

    added better timing and delay, made welding smaller

--- index.js
+++ index.js
@@ -15,7 +15,15 @@ const sparkGenerator = {
     init: function() {
         this.spark()
     },
-
+    getSibling: function(elem) {
+      let siblings = []
+      let sibling = elem.parentNode.firstChild
+      for (; sibling; sibling = sibling.nextSibling) {
+        if (sibling.nodeType !== 1 || sibling === elem) continue
+        siblings.push(sibling)
+      }
+      return siblings
+    },
     getRandomInt: function(min, max, string = true) {
       min = Math.ceil(min);
       max = Math.floor(max);
@@ -27,27 +35,39 @@ const sparkGenerator = {
 
     spark: function() {
        const sparkElements = document.querySelectorAll('.spark')
+
        sparkElements.forEach(spark => {
+
+        let sibling = sparkGenerator.getSibling(spark)
+        let baseAnimationDelay = sparkGenerator.getRandomInt(3,8)
+        console.log(baseAnimationDelay)
+        console.log(sibling)
+        sibling[0].style.animationDelay = String(baseAnimationDelay) +"s"
+
         sparkCount = 25
         for( var i = 0; i <=sparkCount; i++){
-            sparkDiv = sparkGenerator.generateSpark()
+            sparkDiv = sparkGenerator.generateSpark(baseAnimationDelay)
             spark.appendChild(sparkDiv)
         }
        })
     },
-    generateSpark: function() {
+    generateSpark: function(delay) {
         let sparkDiv = document.createElement('div')
+        // set standard properties
         sparkDiv.classList = 'particle'
+        sparkDiv.style.top = sparkGenerator.getRandomInt(10,30) + "px"
+        sparkDiv.style.left =  sparkGenerator.getRandomInt(-3,10) + "px"
+        sparkDiv.style.width = sparkGenerator.getRandomInt(1,2) + "px"
+        sparkDiv.style.height = sparkGenerator.getRandomInt(4,7) + "px"
+        // make some uniqness
         if(sparkGenerator.getRandomInt(1,3) == 2){
           sparkDiv.classList = sparkDiv.classList + " negative-X"
         } else {
           sparkDiv.classList = sparkDiv.classList + " positive-X"
         }
-        sparkDiv.style.top = sparkGenerator.getRandomInt(10,50) + "px"
-        sparkDiv.style.left =  sparkGenerator.getRandomInt(0,25) + "px"
-        sparkDiv.style.width = sparkGenerator.getRandomInt(1,2) + "px"
-        sparkDiv.style.height = sparkGenerator.getRandomInt(4,7) + "px"
-        sparkDiv.style.animationDelay = String(sparkGenerator.getRandomInt(0,9)/10) + "s" 
+        //create a base delay
+        let combinedDelay = (sparkGenerator.getRandomInt(0,9)/10) + parseFloat(delay)
+        sparkDiv.style.animationDelay = String(combinedDelay) + "s" 
         return sparkDiv   
     }
 }
