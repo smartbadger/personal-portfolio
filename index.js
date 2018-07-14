fdf58fb0a9d0efb8bcef35bc10b273206c8348f3
PopSockets <web@popsockets.com>
2018-07-14 14:11:01 -0600

    added sliders for page

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const animationManager = {
         this.startIntervalCounter()
     },
     getRandomInt: function(min, max, string = true) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
+      min = Math.ceil(min)
+      max = Math.floor(max)
       if(string){
         return String(Math.floor(Math.random() * (max - min)) + min)
       }
@@ -62,7 +62,59 @@ const animationManager = {
       }, 45000)
     }
 }
+
+const slideManager = {
+
+  currentSlide: document.getElementById('intro'),
+  list: [],
+  nextSlide: false,
+
+  init: function() {
+    this.buildSlider()
+    this.startAnimation('animation__fade-in')
+  },
+  buildSlider: function() {
+    document.querySelectorAll('.page-widget__section').forEach((element)=> {
+      const bindingElement = document.querySelector(`li[data-bind="${element.id}"]`)
+      if(bindingElement) { 
+        bindingElement.addEventListener('click', this.slideHandler(element, bindingElement))
+        element.addEventListener('animationend', this.progresSlide())
+        this.list.push(bindingElement)
+      }
+    })
+    
+  },
+  clearList: function(array){
+    for (let i = 0; i < array.length; i++){
+      array[i].classList.remove('active')
+    }
+  },
+  slideHandler: function(element, bindingElement) {
+    return ( function () {
+      slideManager.startAnimation('animation__slide-out')
+      slideManager.currentSlide = element
+      slideManager.clearList(slideManager.list)
+      slideManager.nextSlide = true
+      bindingElement.classList.add('active')
+    })
+  },
+  startAnimation: function(animation){
+    this.currentSlide.classList.remove('animation__fade-in')
+    this.currentSlide.classList.remove('animation__slide-in')
+    this.currentSlide.classList.remove('animation__slide-out')
+    this.currentSlide.classList.add(animation)
+  },
+  progresSlide: function() {
+    return( function() {
+      if(slideManager.nextSlide){
+        slideManager.startAnimation('animation__slide-in')
+        slideManager.nextSlide = false;
+      }
+    })
+  }
+}
 document.addEventListener("DOMContentLoaded", evt => {
-	animationManager.init()
+  animationManager.init()
+  slideManager.init()
 })
 
