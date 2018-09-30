fca92aca7802d6703f076e3486b0464853ce2c39
smartbadger <connorbailes@gmail.com>
2018-09-29 18:19:09 -0600

    major gutting, first stage for chart

--- index.js
+++ index.js
@@ -13,48 +13,6 @@ const animationManager = {
       }
       return Math.floor(Math.random() * (max - min)) + min
     },
-
-    buildElements: function() {
-       const sparkElements = document.querySelectorAll('.spark__item')
-       const weldElements = document.querySelectorAll('.weld__container')
-
-       sparkElements.forEach((spark, index) => {
-
-        // create weld item first
-        let sibling = weldElements[index]
-        let baseAnimationDelay = animationManager.getRandomInt(1,15)
-        let weld = document.createElement('div')
-        weld.classList = "weld__item"
-        weld.style.animationDelay = String(baseAnimationDelay) + "s"
-        sibling.appendChild(weld)
-
-        // sparks start here
-        sparkCount = 25
-        for( var i = 0; i <=sparkCount; i++){
-            sparkDiv = animationManager.generateSpark(baseAnimationDelay)
-            spark.appendChild(sparkDiv)
-        }
-       })
-    },
-    generateSpark: function(delay) {
-        let sparkDiv = document.createElement('div')
-        // set standard properties
-        sparkDiv.classList = 'particle'
-        sparkDiv.style.top = animationManager.getRandomInt(25,35) + "px"
-        sparkDiv.style.left =  animationManager.getRandomInt(0,5) + "px"
-        sparkDiv.style.width = animationManager.getRandomInt(1,2) + "px"
-        sparkDiv.style.height = animationManager.getRandomInt(4,7) + "px"
-        // make some uniqness
-        if(animationManager.getRandomInt(1,3) == 2){
-          sparkDiv.classList = sparkDiv.classList + " particle__negative--X"
-        } else {
-          sparkDiv.classList = sparkDiv.classList + " particle__positive--X"
-        }
-        //create a base delay
-        let combinedDelay = (animationManager.getRandomInt(0,9)/10) + parseFloat(delay)
-        sparkDiv.style.animationDelay = String(combinedDelay) + "s" 
-        return sparkDiv   
-    },
     startIntervalCounter: function() {
       setInterval( () => {
         materialSVG = document.getElementById('material-group')
@@ -116,7 +74,7 @@ const slideManager = {
 console.log('Starting');
 document.addEventListener("DOMContentLoaded", evt => {
   console.log('loadedDOM')
-  animationManager.init()
-  slideManager.init()
+  // animationManager.init()
+  // slideManager.init()
 })
 
